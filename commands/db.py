9a77f1dfa01e203cbee557c567054e2ce68419b5
熊大如如 <xxrBear@gmail.com>
2025-07-04 15:19:40 +0800

    Cli db命令改为group命令

--- commands/db.py
+++ commands/db.py
@@ -3,31 +3,39 @@ import click
 from db.setup import clear_table, delete_row, drop_db, init_db
 
 
-@click.command(name="db")
-@click.option("-i", "--init-db", "do_init", is_flag=True, help="初始化数据库表结构")
-@click.option("-d", "--drop-db", "do_drop", is_flag=True, help="删除数据库表结构")
-@click.option("--clear", "clear", is_flag=True, help="删除所有数据库表记录")
-@click.option(
-    "-del",
-    "--delete",
-    "delete",
-    type=click.IntRange(1, None),  # 限制为 >=1，无上限
-    help="删除指定天数前的数据",
-)
-def cli_db(do_init, do_drop, delete, clear):
+@click.group(name="db")
+def cli_db():
     """执行数据库操作"""
-    if do_init:
-        init_db()
-    elif do_drop:
-        confirm = click.confirm("确定要删除所有数据库表？此操作不可恢复！")
-        if not confirm:
-            click.secho("操作取消", fg="yellow")
-        drop_db()
-    elif delete:
-        count = delete_row(delete)
-        click.secho(f"已删除{delete}天前的数据共{count}条", fg="green")
-    elif clear:
-        count = clear_table()
-        click.secho(f"已删除所有数据表记录共{count}条", fg="green")
+    pass
+
+
+@cli_db.command("init")
+def init():
+    """初始化数据库表"""
+    init_db()
+
+
+@cli_db.command("drop")
+def drop():
+    """删除数据库表结构"""
+    confirm = click.confirm("确定要删除所有数据库表？此操作不可恢复！")
+    if not confirm:
+        click.secho("操作取消", fg="yellow")
     else:
-        click.secho("请输入参数 -i（初始化）或 -d（删除）", fg="yellow")
+        drop_db()
+        click.secho("操作完成", fg="green")
+
+
+@cli_db.command("clear")
+def clear():
+    """清空数据表所有数据"""
+    count = clear_table()
+    click.secho(f"已删除所有数据表记录共{count}条", fg="green")
+
+
+@cli_db.command("delete")
+@click.argument("days", type=click.IntRange(1, None))
+def delete(days):
+    """删除指定天数前的数据库记录"""
+    count = delete_row(days)
+    click.secho(f"已删除{days}天前的数据共{count}条", fg="green")
